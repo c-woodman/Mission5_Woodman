3a588ed29ec2406261615f9acf941d59f2c73537
c-woodman <cwoodma1@byu.edu>
2024-02-09 16:46:02 -0700

    Add project files.

---
+++ MyFirstASPNetWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstASPNetWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MyFirstASPNetWebApp/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$("#btnSubmit").click(function () {
+
+    var miles = parseFloat($("#milesInput").val());
+    var chargePerHour = parseFloat($("#chargePerHour").val());
+
+    if (!isNaN(miles) && miles > 0) {
+        var quote = miles * chargePerHour;
+
+        //passing back to my view
+        $("#totalPrice").val(quote.toFixed(2));
+
+    }
+    else {
+        alert("Please enter a valid number of miles greater than 0.");
+        $("#milesInput").val(''); // Clear the totalPrice input
+    }
+
+
+})
